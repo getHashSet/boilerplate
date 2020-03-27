bca7af2bf460573e1c9dce97f4e64443940aa5da
Matthew <matthew_carpenter@icloud.com>
2020-03-27 14:49:52 -0700

    Updated readme to contain store instructions.

--- README.md
+++ README.md
@@ -25,6 +25,7 @@ Custom React Boilerplate.
 * redux
 * react-redux
 * aws-sdknpm
+* Google Chrome extension Redux DevTools *https://github.com/zalmoxisus/redux-devtools-extension*
 
 ## Get Started
 
@@ -33,4 +34,61 @@ npx create-react-app
 npm install redux react-redux
 ```
 
-## .gitignore
+## .gitignore & .env
+
+Be sure to use the .env file in the root directory.
+
+## Setup a new item in the store
+
+store is set up in the index.js file within boilerplate>src>index.js
+
+Step 1: Go to reducers folder and create a new reducer folder and index file. `boilerplate>src>reducers>`;
+
+Step 2: `create new folder` `add index.js to that folder` *(example: isLoggedIn.js)*
+
+Step 3: Using the following code as a template you must set the default state of the new object.
+
+```javascript
+const loggedReducer = (state = false, action) => {
+    switch(action.type){
+        case 'SIGN_IN':
+            return !state;
+        default:
+            return state;
+    };
+};
+
+export default loggedReducer;
+```
+
+Be sure to add a default value.
+
+Step 4: Navigate to the reducers index file. `boilerplate>src>reducers>index.js`
+
+Step 5: import your new state file.
+```javascript
+// this file can have any name you would like. I have chosen loggedReducer as an example.
+import loggedReducer from './isLoggedIn';
+```
+
+Step 6: Add this variable to the default store object.
+```javascript
+// option 1
+// the value within the state is now 'counter: 0, isLoggedIn: false'
+const allReducers = combineReducers({
+    counter: counterReducer,
+    isLoggedIn: loggedReducer
+});
+```
+
+```javascript
+// option 2 
+// using ES6 you can create a default value within the combineReducers function.
+// the value within the state is now 'counterReducer: 0, loggedReducer: false'
+const allReducers = combineReducers({
+    counterReducer,
+    loggedReducer
+});
+```
+
+Step 7: Verify default state in Google Chrome.
--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -3,40 +3,50 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
+import allReducers from './reducers'; // this will import all reduceres that are imported into the index.js file in the reducers folder
 
-// STORE -> GLOBALIZED STATE
+// create store and add chrome extension tools for debuging via chrome. source: https://github.com/zalmoxisus/redux-devtools-extension
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-// ACTION -> A function that returns an object.
 
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  };
-};
-
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  };
-};
-
-// REDUCER -> CHECKS TO UPDATE STORE
-const counter = (state = 0, action) => {
-  switch(action.type){
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  };
-};
-
-let store = createStore(counter);
-
-// Display it in the console.
-store.subscribe(() => console.log(store.getState()));
-
-// DISPATCH -> SENDS ACTIONS TO THE REDUCER
-store.dispatch(increment());
+////////////////////////////////////////////////////////////////////////////
+// example without breaking up components
+////////////////////////////////////////////////////////////////////////////
+// // STORE -> GLOBALIZED STATE
+//
+// // ACTION -> A function that returns an object.
+//
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT'
+//   };
+// };
+//
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT'
+//   };
+// };
+//
+// // REDUCER -> CHECKS TO UPDATE STORE
+// const counter = (state = 0, action) => {
+//   switch(action.type){
+//     case "INCREMENT":
+//       return state + 1;
+//     case "DECREMENT":
+//       return state - 1;
+//   };
+// };
+//
+// let store = createStore(counter);
+//
+// // Display it in the console.
+// store.subscribe(() => console.log(store.getState()));
+//
+// // DISPATCH -> SENDS ACTIONS TO THE REDUCER
+// store.dispatch(increment());
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
 
 // This will render <App /> from ./App.js;
 ReactDOM.render( <App />, document.getElementById('root'));
