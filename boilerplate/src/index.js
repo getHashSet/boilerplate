df68adaa92f1df4fe19fee2b4589a3b2c323f56b
Matthew <matthew_carpenter@icloud.com>
2020-03-27 15:12:03 -0700

    updated store and Provider component.

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
 import allReducers from './reducers'; // this will import all reduceres that are imported into the index.js file in the reducers folder
+import { Provider } from 'react-redux'; // this will grant access to anything within the <Provider></Provider> Elements. Place around <App /> Component to grant access to your whole app.
 
 // create store and add chrome extension tools for debuging via chrome. source: https://github.com/zalmoxisus/redux-devtools-extension
 const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
@@ -49,7 +50,12 @@ const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && wi
 ////////////////////////////////////////////////////////////////////////////
 
 // This will render <App /> from ./App.js;
-ReactDOM.render( <App />, document.getElementById('root'));
+ReactDOM.render( 
+  <Provider store={store}> {/* This takes one attribute. store={theNameOfYourStore} */}
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
