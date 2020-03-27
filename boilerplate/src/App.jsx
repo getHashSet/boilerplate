acf6ce6c72560d73d77a06932705d1749ffabcbe
Matthew <matthew_carpenter@icloud.com>
2020-03-27 15:27:23 -0700

    Store is working as expected. Example includes pulling actions and store into App.jsx.

---
+++ boilerplate/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
+
+function App() {
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+      <h2>{counter}</h2>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </div>
+  );
+}
+
+export default App;
