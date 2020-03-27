24172b21988777e69650a0247f77b8847fbf5f1c
Matthew <matthew_carpenter@icloud.com>
2020-03-27 12:18:16 -0700

    updated store example in index.js

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -2,10 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
 
-// STORE
+// STORE -> GLOBALIZED STATE
 
+// ACTION -> A function that returns an object.
 
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  };
+};
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  };
+};
+
+// REDUCER -> CHECKS TO UPDATE STORE
+const counter = (state = 0, action) => {
+  switch(action.type){
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  };
+};
+
+let store = createStore(counter);
+
+// Display it in the console.
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH -> SENDS ACTIONS TO THE REDUCER
+store.dispatch(increment());
 
 // This will render <App /> from ./App.js;
 ReactDOM.render( <App />, document.getElementById('root'));
